7d78055de6206c84fc039242ad4cecf0ec98c97a
Mirkomil Mirvohidov <mirkomilmirvohidov4@gmail.com>
2024-06-21 20:55:23 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+let hafta = prompt("Hafta kunlri")
+
+switch (hafta) { 
+    case "1":
+        alert("1 ->dushamba") ;
+        break;
+    case "2":
+        alert("2 ->seyshanba");
+        break;
+    case "2":
+        alert("3 ->payshanbas");
+        break;
+    default:
+    alert ("bunday hafta yoq")                         
+}
+
+// if (hafta === "mondey") {
+//     alert (`mondey ->dushamba`)
+// } else if (hafta === "tuesday") {
+//     alert (`tuesday ->seyshanba`)
+// } else if (hafta === "wendesday") {
+//     alert (`wendesday ->chorshanba`)
+// } 
